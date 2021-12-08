0ffb1ed3b27669d392115581210a37276bbd3617
ralucaborca <raluca.borca09@gmail.com>
2021-12-08 17:08:39 +0200

    Main problems fixed.

--- app/src/main/java/com/example/heartrateapplication/MainActivity.java
+++ app/src/main/java/com/example/heartrateapplication/MainActivity.java
@@ -2,28 +2,17 @@ package com.example.heartrateapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private TextView register;
+
+public class MainActivity extends AppCompatActivity{
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
-        register = (TextView) findViewById(R.id.signup);
-        register.setOnClickListener(this);
-    }
+        setContentView(R.layout.activity_main);
 
-    public void onClick(View v){
-        switch (v.getId()){
-            case R.id.signup:
-            startActivity(new Intent(this,SignUp.class));
-            break;
-        }
     }
 }
